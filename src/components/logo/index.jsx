c38df45c759592727cad689ee7071c5f3a4ed053
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2023-07-02 03:04:11 +0200

    updated reference to logo image

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import * as S from "./style";
+import logoDesktop from "../../static/img/logo.png";
+import logoMobile from "../../static/img/logo-mob.png";
+
 export default function Logo() {
   return (
     <React.Fragment>
       <S.Logo>
-        <S.LogoImg src="./logo.png" alt="logo"></S.LogoImg>
+        <S.LogoImg src={logoDesktop} alt="logo"></S.LogoImg>
       </S.Logo>
       <S.LogoMobile>
-        <S.LogoImgMobile src="./logo-mob.png" alt="logo" />
+        <S.LogoImgMobile src={logoMobile} alt="logo" />
       </S.LogoMobile>
     </React.Fragment>
   );
