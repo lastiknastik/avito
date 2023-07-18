5a674daa019d5e25d79b654b8b8fc923395fdaa6
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2023-07-18 23:39:21 +0200

    refactored logo component - made it to redirect to main page on click

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -2,15 +2,31 @@ import React from "react";
 import * as S from "./style";
 import logoDesktop from "../../static/img/logo.png";
 import logoMobile from "../../static/img/logo-mob.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Logo() {
+  const navigate = useNavigate();
+  const onLogoClickHandler = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    navigate("/");
+  };
+
   return (
     <React.Fragment>
       <S.Logo>
-        <S.LogoImg src={logoDesktop} alt="logo"></S.LogoImg>
+        <S.LogoImg
+          src={logoDesktop}
+          alt="logo"
+          onClick={onLogoClickHandler}
+        ></S.LogoImg>
       </S.Logo>
       <S.LogoMobile>
-        <S.LogoImgMobile src={logoMobile} alt="logo" />
+        <S.LogoImgMobile
+          src={logoMobile}
+          alt="logo"
+          onClick={onLogoClickHandler}
+        />
       </S.LogoMobile>
     </React.Fragment>
   );
